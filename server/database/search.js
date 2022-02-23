04bc10a740ddb2e08a75f0f5c0f642570bdd8573
Kyle <KyleGSoftware@gmail.com>
2022-02-23 18:44:13 -0500

    Express API queries are working, first route generated

--- server/database/search.js
+++ server/database/search.js
@@ -1,8 +1,8 @@
 export async function findOneItemBySKU(client, productSKU) {
     const result = await client.db("pottery").collection("inventory").findOne({ProductSKU: productSKU});
-
     if (result) {
         console.log(`Item found with SKU: ${productSKU}`);
+        return(result);
     } else {
         console.log(`No item found with SKU: ${productSKU}`);
     }
--- server/server.js
+++ server/server.js
@@ -2,19 +2,20 @@
 import { MongoClient } from 'mongodb'
 import express from 'express';
 import * as search from './database/search.js';
+import 'dotenv/config'
 
-
-// import connection.js exports
-import * as connection from './database/connection.js'
 
 // init mongodb module
 const connectionString = process.env.ATLAS_URI;
+console.log(`connectionString = ${process.env.ATLAS_URI}`);
 const client = await new MongoClient(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const res = await connection.MongoConnect();
+async function main() {
+  MongoConnect()
+}
 
 
 // init express
@@ -25,8 +26,10 @@ const port = process.env.PORT || 4000;
 
 
 app.get('/item/:sku', async (req, res) => {
+  //HTTP queries and path variables are sent as a string - we need to cast req.params.sku into an int
+  req.params.sku = parseInt(req.params.sku);
   const result = await search.findOneItemBySKU(client, req.params.sku)
-  res.send(result)
+  res.json(result)
 })
 
 
@@ -34,3 +37,19 @@ app.get('/item/:sku', async (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+
+
+async function MongoConnect() {
+  try {
+    await client.connect();
+  } catch (e) {
+    console.error(e);
+  } finally {
+    //await client.close();
+  }
+  
+}
+
+main();
